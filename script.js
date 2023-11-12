2ee6d102d83f1d41b8e67b3d74e4940bb1efe864
Abdullah sajid <abdullahsajid521@gmail.com>
2023-11-12 15:54:25 +0500

    -adding animation scroll

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const nav = document.querySelector('.navbar')
+const swup = new Swup();
 const mobNav = document.querySelector('.mob-nav')
 const hamburger = document.querySelector('.hamburger')
-window.addEventListener('scroll',()=>{
-    if(window.scrollY > 100){
-        nav.style.backdropFilter = "blur(10px)"
-    }
-})
 
 hamburger.addEventListener('click',()=>{
     mobNav.classList.toggle('mob-nav-active')
 })
+
+
+const observer = new IntersectionObserver((entities)=>{
+    // console.log("entities",entities)
+    entities.forEach((entry)=>{
+        // console.log("entry",entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const getEle = document.querySelectorAll('.hidden')
+getEle.forEach((item) => observer.observe(item))
